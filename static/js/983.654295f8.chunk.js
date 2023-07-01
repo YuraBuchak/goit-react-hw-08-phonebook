c30ac97a591422f327ba5040a6cf62669921e8f4
YuraBuchak <115108617+YuraBuchak@users.noreply.github.com>
2023-07-01 13:22:48 +0000

    Deploying to gh-pages from @ YuraBuchak/goit-react-hw-08-phonebook@3e26210f3e1a3644cd27cb22badbf06bca3f7def 🚀

---
+++ static/js/983.654295f8.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkreact_homework_template=self.webpackChunkreact_homework_template||[]).push([[983],{3983:function(e,t,r){r.r(t);var a=r(184);t.default=function(){return(0,a.jsx)("div",{children:"Homepage"})}}}]);
+//# sourceMappingURL=983.654295f8.chunk.js.map
